02df3afa5ed95d6b680845cc4851f2e82bafdeba
JennyYang19 <850341643@qq.com>
2017-04-07 13:11:21 +0800

    dddd

---
+++ code/springboot/src/main/java/com/edu/spring/MyConfig.java
@@ -0,0 +1,14 @@
+package com.edu.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+
+	@Bean
+	public MyBean createMyBean(){
+		return new MyBean();
+	}
+	
+}
