e7ae747527956559d90054cfc8a831336188b48d
JennyYang19 <850341643@qq.com>
2017-04-07 11:05:22 +0800

    2 changes

--- code/springboot/src/main/java/com/edu/spring/App.java
+++ code/springboot/src/main/java/com/edu/spring/App.java
@@ -11,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
        AnnotationConfigApplicationContext context =new AnnotationConfigApplicationContext(MyConfig.class);
+       System.out.println(context.getBean(MyBean.class));
+       context.close();
     }
 }
